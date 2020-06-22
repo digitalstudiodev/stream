3107f6299f3c6a6629d2b935a87ceb28619c4017
jose-dom <josedom000@gmail.com>
2020-06-21 20:04:24 -0400

    update

--- users/views.py
+++ users/views.py
@@ -10,18 +10,18 @@ from .models import User
 def error_view(request):
     if request.user.is_authenticated:
         messages.warning(request,f'Invalid Request. Please report any issue to via email to digitalstudio.dev000@gmail.com')
-        return redirect('blog:home')
+        return render(request, "users/base.html")
     else:
         messages.warning(request,f'Invalid Request. Please Login.')
-        return redirect('users:login')
+        return render(request, "users/base.html")
 
 def error_view_400(request, exception):
     if request.user.is_authenticated:
         messages.warning(request,f'Invalid Request. Please report any issue to via email to digitalstudio.dev000@gmail.com')
-        return redirect('blog:home')
+        return render(request, "users/base.html")
     else:
         messages.warning(request,f'Invalid Request. Please Login.')
-        return redirect('users:login')
+        return render(request, "users/base.html")
 
 def register(request):
     form = UserRegisterForm()
