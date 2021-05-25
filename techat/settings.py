7679ab3cfb9d2d56b320f33d1a2f93ac2dc8be0a
jose-dom <josedom000@gmail.com>
2021-05-25 11:30:26 -0400

    update visuals

--- techat/settings.py
+++ techat/settings.py
@@ -154,6 +154,7 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+
 #HTTPS settings
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
