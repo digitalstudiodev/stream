b4b039e55e9e39c3a19c6d0a7b24065015e78890
jose-dom <jrd251@rutgers.edu>
2021-10-03 18:53:22 -0400

    update urls

--- techat/urls.py
+++ techat/urls.py
@@ -39,3 +39,6 @@ if settings.DEBUG:
     urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+handler404 = 'users.views.invalid_view'
+handler500 = 'users.views.invalid_error'
