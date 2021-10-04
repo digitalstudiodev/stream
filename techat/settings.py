d3e5bfdfd0118a6d500f3fced6457913f0d2a3e1
jose-dom <jrd251@rutgers.edu>
2021-10-03 23:54:38 -0400

    update settings

--- techat/settings.py
+++ techat/settings.py
@@ -147,6 +147,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 #AWS Config Settings
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
