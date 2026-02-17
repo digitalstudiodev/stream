462a9579b94daa077f8f225aa897258ba3c6b977
Jose <jrd251@rutgers.edu>
2026-02-17 15:51:17 -0500

    update

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from .views import (PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, TagPostListView, feed, main)
+from .views import (PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, TagPostListView, feed, main, resume)
 
 app_name = 'blog'
 
 urlpatterns = [
     path('feed/', feed, name='feed'),
     path('', main, name='main'),
+    path('resume/', resume, name='resume'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('content/', PostListView.as_view(), name='home'),
     path('posts/<str:tag>/', TagPostListView.as_view(), name='posts-by-tag'),
